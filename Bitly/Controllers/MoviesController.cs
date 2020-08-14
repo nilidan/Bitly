4ca7f0de43ab2ad72b64a80dc0337b8bc6dd8653
Nino Ilidan <nino.ilidan@okstate.edu>
2020-08-13 22:41:20 -0700

    Initial  Project

---
+++ Bitly/Controllers/MoviesController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Bitly.Models;
+
+namespace Bitly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "The Shadow" };
+            return View(movie);
+        }
+    }
+}
